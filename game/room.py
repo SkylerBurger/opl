d8d138b8da146dadf716cbb53351c9083bcb7f30
Skyler Burger <skylerburger@gmail.com>
2022-08-27 19:25:44 -0700

    Adds rooms

---
+++ game/room.py
@@ -0,0 +1,17 @@
+from mechanic import Mechanic
+
+
+class Room:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        mechanic: Mechanic | None = None,
+    ):
+        self.name = name
+        self.description = description
+        self.mechanic = mechanic
+        self.neighbors = {"north": None, "east": None, "south": None, "west": None}
+
+    def play(self, player):
+        self.mechanic.play(player)
